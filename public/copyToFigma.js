194db70a3ea704a8de07313f704d15f2c0fa8818
gregoire charles <gcm.charles@gmail.com>
2024-05-09 17:13:59 +0200

    initial commit

---
+++ public/copyToFigma.js
@@ -0,0 +1,49 @@
+function copyToFigma(event) {
+
+    // get the copy button
+    const copyButton = document.getElementById(event.target.id);
+
+    // change button text to inform user operation in progress
+    copyButton.textContent = "Copied to clipboard";
+
+
+    fetch(`http://localhost:3000/figma/code/${event.target.id}.txt`)
+    .then(response => response.text())
+    .then(data => {
+        if (data) {
+            navigator.clipboard.write([
+                new ClipboardItem({
+                    'text/html': new Blob([data], {type: 'text/html'})
+                })
+            ]).then(() => {
+                console.log("Figma code successfully copied to clipboard.");
+            }).catch(err => {
+                console.error("Failed to copy Figma code: ", err);
+            });
+
+        } else {
+            console.log('Code not available');
+        }
+    })
+    .catch(error => {
+        console.log('Error fetching figma-code:', error);
+    });
+
+
+    //after 1 second, set button text back
+    setTimeout(() => {
+      copyButton.textContent = "Copy to Figma";
+    }, 1500);
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function(event) {
+        if (event.target.className === 'copy-button') {
+            copyToFigma(event)
+        }
+    });
+});
+
+
+
